e39a52a873d307f5f3312b7b7754a116ac73b537
nukimo <email>
2026-06-28 00:16:55 +0700

    fix n+1 problem

--- src/main/java/com/example/first_project/repository/TaskRepository.java
+++ src/main/java/com/example/first_project/repository/TaskRepository.java
@@ -1,7 +1,13 @@
 package com.example.first_project.repository;
 
 import com.example.first_project.entity.Task;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    @EntityGraph(attributePaths = "tags")
+    List<Task> findAll();
 }
